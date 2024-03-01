53b838d696aac439bab8a689f86a3987936169b8
egimbaeva22 <alinaegimbaeva2006@gmail.com>
2024-03-01 18:53:40 +0600

    dz6

---
+++ src/store/products/actions/productGetActions.js
@@ -0,0 +1,7 @@
+
+import dataJson from '../../../data/product.json'
+export const fetchData =() => {
+    return async (dispatch) => {
+        dispatch({type:'FETCH_DATA', payload:dataJson})
+    }
+}
---
+++ src/store/products/busketReducer/actionBusket.js
@@ -0,0 +1,6 @@
+
+export const actionBusket = (product) => {
+    return async (dispatch) => {
+        dispatch({type: 'ADD_BUSKETS', payload: product})
+    }
+}
